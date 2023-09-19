e29aef9a4007358c01baef746a29c22a62f7464b
Muhammad Bilal <bilalshahidvayani@gmail.com>
2023-09-19 15:12:44 +0500

    java4task

---
+++ app.js
@@ -0,0 +1,70 @@
+// Task 1: 
+for (var i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+
+// Task 2: 
+for (var i = 0; i <= 11; i++) {
+    console.log(i)
+}
+
+
+// Task 3: 
+for (var i = 0; i <= 4; i++) {
+   
+}
+
+
+// Task 4: 
+for (var j = 0; j < 100; j++) {
+    }
+// Task 5: 
+for (var i = 3; i > 0; i--) {
+    
+}
+
+// Task 6: 
+var arrayLength = myArray.length;
+
+// Task 7: 
+var flag = true; 
+
+// Task 8
+for (var i = 0; i < pets.length; i++){
+
+}
+
+
+// Task 9:
+
+
+// task 10 
+var userNames = ["John", "Jane", "Bob", "Alice"];
+
+var firstName = prompt("Enter first name");
+var matchFound = false;
+for (var i = 0; i < userNames.length; i++) {
+    if (firstName === userNames[i]) {
+        alert("Match found");
+        matchFound = true;
+        break;
+    }
+}
+if (!matchFound) {
+    alert("Please write correct user name");
+}
+// task 11
+var matchFound = false;
+for (var i = 0; i < list.length; i++) {
+    if (userInput === list[i]) {
+        alert("Match found");
+        matchFound = true;
+        break;
+    }
+}
+if (!matchFound) {
+    alert("Match not found");
+}
+
+// task 12
